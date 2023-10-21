8e04c16d61522562429d7041d720bfc9e6bb91dc
kingakshat <akshatshrsat77@gmail.com>
2023-10-21 12:12:12 +0530

    added HTML scripts

--- main_app.py
+++ main_app.py
@@ -1,6 +1,8 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
-from flask import Flask
+from flask import Flask, render_template
+
+
  
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
@@ -12,7 +14,7 @@ app = Flask(__name__)
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
-    return 'Hello World'
+    return render_template('index.html')
  
 # main driver function
 if __name__ == '__main__':
